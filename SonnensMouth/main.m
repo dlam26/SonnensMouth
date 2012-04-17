43756b6d73b1b3e7b002863c2299c9de156ed71f
David Lam <d@dlam.me>
2012-04-17 00:19:58 -0700

    how to get a stacktrace for a SIGABRT

--- SonnensMouth/main.m
+++ SonnensMouth/main.m
@@ -13,6 +13,17 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        
+        //originally was just this:
+        
+//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        
+        //print stack trace for SIGABRT http://stackoverflow.com/questions/9122663/how-can-i-fix-this-sigabrt-error-with-my-app
+        @try {
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        } @catch (NSException *e) {
+            NSLog(@"CRASH: %@", e);
+            NSLog(@"Stack Trace: %@", [e callStackSymbols]);
+        }
     }
 }
