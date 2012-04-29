374011559e148fe9bac8bb4d46f6de3d6a605c96
David Lam <d@dlam.me>
2012-04-29 01:12:51 -0700

    added some tabbar icons i scavanged for on the internet

--- SonnensMouth/TweetsViewController.h
+++ SonnensMouth/TweetsViewController.h
@@ -6,8 +6,15 @@
 //  Copyright (c) 2012 dlam.me. All rights reserved.
 //
 
+//  4/29/12  whoa, Apple has built-in twitter frameowrk?! 
+//      http://developer.apple.com/library/ios/#samplecode/Tweeting/Introduction/Intro.html
+//      https://dev.twitter.com/docs/ios
+
 
 #import <UIKit/UIKit.h>
+
+//#import <Twitter/TWRequest.h>
+//#import <Twitter/TWTweetComposeViewController.h>
 
 #import "SonnensMouth.h"
 #import "SaidSoViewController.h"
